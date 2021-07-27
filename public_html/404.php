8eca28635d1f10d63d66f62fd1877cec5d777d67
realashleybailey <admin@zeplinwarriors.com>
2021-07-27 04:17:30 +0100

    core

--- public_html/404.php
+++ public_html/404.php
@@ -1,7 +1,26 @@
 <?php
 
-echo __FILE__;
+echo "*\n<br>";
+echo "* Created on Tue Jul 27 2021\n<br>";
+echo "*\n<br>";
+echo "* The MIT License (MIT)\n<br>";
+echo "* Copyright (c) 2021 Uhppote PHP\n<br>";
+echo "* Created by Ashley Bailey\n<br>";
+echo "*\n<br>";
+echo "* Permission is hereby granted, free of charge, to any person obtaining a copy of this software\n<br>";
+echo "* and associated documentation files (the \"Software\"), to deal in the Software without restriction,\n<br>";
+echo "* including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,\n<br>";
+echo "* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,\n<br>";
+echo "* subject to the following conditions:\n<br>";
+echo "*\n<br>";
+echo "* The above copyright notice and this permission notice shall be included in all copies or substantial\n<br>";
+echo "* portions of the Software.\n<br>";
+echo "*\n<br>";
+echo "* THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED\n<br>";
+echo "* TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL\n<br>";
+echo "* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,\n<br>";
+echo "* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n<br>";
+echo "*\n<br>";
 
-echo "<print>";
-print_r($_SERVER);
-echo "</print>";
+?>
+<h1>404</h1>
