ce5c9d8e733b31739405c3bb72b3b3c3e442b2a4
Ashley Bailey <admin@zeplinwarriors.com>
2021-08-01 16:13:02 +0100

    update

--- public_html/pull.php
+++ public_html/pull.php
@@ -1,7 +1,6 @@
 <?php
 
 if (isset($_POST['payload']) || isset($_GET['payload'])) {
-
     exec('cd /var/www/Uhppote-PHP/ && git reset --hard HEAD && git pull', $output);
     echo json_encode($output);
 }
