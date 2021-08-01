5e194c10203084352b6cde6a60d6fc643bd3b7b9
Ashley Bailey <admin@zeplinwarriors.com>
2021-08-01 16:17:15 +0100

    update

--- public_html/login.php
+++ public_html/login.php
@@ -28,6 +28,12 @@ if (isset($_SESSION['LOGGED_IN']) && $_SESSION['LOGGED_IN'] == true) {
     header('Location: /admin');
 }
 
+echo "gas";
+echo "gas";
+echo "gas";
+echo "gas";
+echo "gas";
+echo "gas";
 ?>
 <style>
     html,
